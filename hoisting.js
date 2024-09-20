dbe26fc59315412af3af3173fa48e7c02f61d469
Sri Harsha <sri_harsha509@hotmail.com>
2024-09-19 20:45:49 -0400

    19th sep

---
+++ hoisting.js
@@ -0,0 +1,45 @@
+
+if (true) {
+    var a2 = 10;
+}
+
+console.log(a2)
+
+
+
+
+// for loop
+
+
+/*
+for (delcaration, condition, increment/decrement) {
+ ...statements
+ }
+*/
+
+
+
+let name = "harsha";
+
+for (let i= (name.length)-1; i>= 0; i--) {
+
+    console.log(name.charAt(i));
+}
+
+
+/*
+
+ i = 1; 1 < 10 = true
+       console.log("i am in for "+ 1)
+       for = > i++
+
+
+
+
+
+ */
+
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,74 @@
+
+// && and ||
+let a = 10, b= 1;
+if (a> 0 && b> 0 ) {
+    console.log(a, b);
+}
+
+let c = 0, d = 3;
+if (c> 0 || d >1) {
+    console.log(d);
+}
+
+
+let flag = false;
+
+if(!flag) {
+    console.log("execute code");
+}
+
+let value = null;
+let value1;
+
+if(!value1) {
+    value1 = 10;
+    console.log(value1);
+}
+
+//---------------------------
+
+let name = "harsha"; // h=0, a= 1, |||  6
+
+//while
+// for loops
+
+let i = 1;
+
+// while (condition)  {
+// ...statements
+// }
+
+while (i > 0 && i < 10) {
+    console.log('i am at' + i);
+    i++;
+}
+
+
+
+
+
+if(true) { // blocked scope
+    let a1 = 0
+    console.log(a1)
+}
+
+
+// console.log(a1);
+
+if (true) {
+    var a2 = 10;
+}
+
+
+console.log(a2)
+
+
+
+
+
+
+
+
+
+
+
